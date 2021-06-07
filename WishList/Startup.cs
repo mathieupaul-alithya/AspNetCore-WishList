cd00498cc76306ec9ab391cd669344cb6c41ad4a
Mathieu Paul Le Clerc <mathieupaul.leclerc@alithya.com>
2021-06-07 07:55:39 -0400

    Added missing UseEndpoints call

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -24,6 +24,8 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+
             if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
